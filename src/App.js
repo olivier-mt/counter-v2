7eec9dc4f9a77e27859c10cdbbf3cb078780625b
Olivier Mountou <oliviermountou@MacBook-Pro-de-Olivier.local>
2021-04-30 18:08:52 +0200

    waiting for css

--- src/App.js
+++ src/App.js
@@ -3,33 +3,35 @@ import { useState } from "react";
 import Counter from "./Components/Counter";
 
 function App() {
-  const [countersNumber, setCounterNumber] = useState([]);
-  const [counter0, setCounter0] = useState(0);
-  const [counter1, setCounter1] = useState(0);
-  const [counter2, setCounter2] = useState(0);
+  const [countersTab, setCounterNumber] = useState([]);
+  const [counter, setCounter] = useState(0);
 
+  console.log("counterTabs ===>", countersTab);
   const handleOnclick = () => {
-    const newCounterNumber = [...countersNumber];
+    const newCounterNumber = [...countersTab];
     newCounterNumber.push(0);
-
-    // J'essaie essaie ici de créer des nom de variable counter & setCounter personnalisté pour chaque state !
 
     return setCounterNumber(newCounterNumber);
   };
 
-  console.log("counterNumber ===>", countersNumber);
+  console.log("counterNumber ===>", countersTab);
   const tab = [];
   return (
     <div>
-      <button onClick={handleOnclick}>Add counter</button>
-      {countersNumber.map((elem, index) => {
-        if ((index = 0)) {
-          tab.push(<Counter counter={counter0} setCounter={setCounter0} />);
-        } else if ((index = 1)) {
-          tab.push(<Counter counter={counter1} setCounter={setCounter1} />);
-        } else if ((index = 2)) {
-          tab.push(<Counter counter={counter2} setCounter={setCounter2} />);
-        }
+      {countersTab.length < 3 && (
+        <button onClick={handleOnclick}>Add counter</button>
+      )}
+
+      {countersTab.map((elem, index) => {
+        tab.push(
+          <Counter
+            counter={counter}
+            setCounter={setCounter}
+            countersTab={countersTab}
+            setCounterNumber={setCounterNumber}
+            i={index}
+          />
+        );
       })}
       {tab}
     </div>
--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,18 +1,33 @@
-const Counter = ({ counter, setCounter }) => {
-  console.log("counter ===>", counter);
+const Counter = ({ countersTab, setCounterNumber, i }) => {
   const handleMinus = () => {
-    return setCounter(counter - 1);
+    const newTab = [...countersTab];
+    newTab[i] = countersTab[i] - 1;
+
+    return setCounterNumber(newTab);
   };
 
   const handlePlus = () => {
-    return setCounter(counter + 1);
+    const newTab = [...countersTab];
+
+    newTab[i] = countersTab[i] + 1;
+
+    return setCounterNumber(newTab);
+  };
+
+  const handleReset = () => {
+    const newTab = [...countersTab];
+    newTab[i] = 0;
+    return setCounterNumber(newTab);
   };
 
   return (
     <div>
-      <button onClick={handleMinus}>-</button>
-      <p>{counter}</p>
-      <button onClick={handlePlus}>+</button>
+      {countersTab[i] > 0 && <button onClick={handleMinus}>-</button>}
+
+      <p>{countersTab[i]}</p>
+      {countersTab[i] < 10 && <button onClick={handlePlus}>+</button>}
+
+      <button onClick={handleReset}>RESET</button>
     </div>
   );
 };
